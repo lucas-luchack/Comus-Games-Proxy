0c0303995578293a18b4b7e5dcee7357d5b22281
ESPIET L. <espiet.l@valbion.com>
2025-03-04 11:14:54 +0100

    :bug: Correction du lancement du proxy

--- src/server.ts
+++ src/server.ts
@@ -47,8 +47,8 @@ app.use((req, res, next) => {
         res.status(404).send('Application not found');
     }
 });
+*/
 
 app.listen(port, () => {
     console.log(`Proxy server running on http://localhost:${port}`);
 });
-*/
